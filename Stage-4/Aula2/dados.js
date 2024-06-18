fa10a578d9c07fe2d860c4623a9a079f754d1395
Wesley Alexandre <wesley.lopes14@hotmail.com>
2024-06-17 21:02:23 -0300

    continuando_curso

--- Stage-4/Aula2/dados.js
+++ Stage-4/Aula2/dados.js
@@ -1,7 +1,47 @@
-let string = "123";
+/* let string = "123";
 
 console.log(Number(string));
 
 let number = 321;
 
-console.log(String(number));
+console.log(String(number)); */
+
+/* Transformando em string numero e vice VideoColorSpace
+
+let word = "Sonho";
+console.log(word.length);
+
+let number = 3434;
+console.log(String(number).length); */
+
+// ---
+/* let number = 23112112.1212111212;
+console.log(number.toFixed(4).replace(".", ",")); */
+// Acabou virando STRING
+
+//-- Transformar letras minusculas em maiusculas
+/* let word = "Wesley Alexandre";
+console.log(word.toLowerCase().replace("w", "Z")); */
+
+// -- Separe um texto que contem espaços, arros, posição, etc
+
+/* let texto = "Wesley Alexandre Rodrigues Martins";
+let myArray = texto.split(" ");
+console.log(myArray);
+
+let transform = myArray.join("_");
+console.log(transform); */
+
+
+//-- Verficiar se um texto possui uma palavra especifica
+
+/* let texto = "Wesley Alexandre Rodrigues Martins";
+console.log(texto.includes("Rodrigues")); */
+
+// -- Criar Array com construtor
+
+/* let myArray = new Array(4);
+console.log(myArray); */
+
+// -- Contar elementos do Array
+console.log(["a", "b", "c"].length);
