b02db876a29719196eb30d3fa6b85037e038d6e5
Wesley Alexandre <wesley.lopes14@hotmail.com>
2024-12-29 08:32:53 -0300

    Finalizando

--- Stage-5/jogo-adivinhacao/main.js
+++ Stage-5/jogo-adivinhacao/main.js
@@ -3,10 +3,8 @@ const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
 const btnTry = document.querySelector("#btnTry")
 const btnAgain = document.querySelector("#btnAgain")
-
 const randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
-
 // eventos
 btnTry.addEventListener('click', handleTryClick)
 btnAgain.addEventListener('click', handleAgainClick)
@@ -15,20 +13,15 @@ document.addEventListener('keydown', function(enter) {
         handleAgainClick()
     }
 })
-
 // função callback
 function handleTryClick(event) {
     event.preventDefault() // não faça o padrão
     const inputNumber = document.querySelector("#inputNumber")
-    console.log(inputNumber.value)
-
     if(Number(inputNumber.value) == randomNumber) {
         toggleScreen()
         screen2
-            .querySelector("h2").innerText = `acertou em ${xAttempts} tentativas.`
-
-        console.log(`você acertou em ${xAttempts}`)
-    }
+            .querySelector("h2").innerText = `acertou em ${xAttempts} tentativas.`}
+    
     inputNumber.value = ""
     xAttempts++
 }
@@ -41,4 +34,13 @@ function handleAgainClick() {
 function toggleScreen () {
     screen1.classList.toggle("hide")
     screen2.classList.toggle("hide")
+}
+
+function errorColor() {
+    // mudando cor ao errar número
+    document.body.style.background = 'red';
+    // voltando a cor original
+    setTimeout(() => {
+        document.body.style.background = ''; // remove o estilo inline
+    }, 1000)
 }
