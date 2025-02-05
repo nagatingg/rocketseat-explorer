4a98652d44e8cee90cd6ff9d29c8c3a6fc9e538e
Wesley Alexandre <wesley.lopes14@hotmail.com>
2025-02-05 12:35:28 -0300

    Desestruturação em objetos, funções. Rest params. Spreads

--- Stage-5.1/ecmaScript/script.js
+++ Stage-5.1/ecmaScript/script.js
@@ -34,3 +34,52 @@ const [banana] = fruits
 const [_, apple] = fruits
 console.log(apple)
 
+// Desesutruturação em objetos
+const product = {
+    description: 'Mouse',
+    price: 150
+}
+
+const {description, price} = product
+console.log('Descrição:', description)
+
+// Desestruturação em funções
+function newProduct({name, description, price}){
+    console.log('Description:', description)
+}
+// newProduct(_, 'Teclado')
+newProduct({
+    description: 'Teclado'
+})
+
+// Rest params (...) permite representar um número indefnido de argumentos como um array
+function values (a, ...rest){ // comum rest ser chamado de args
+    // Mostra a quantidade de parametros
+    console.log(rest.length)
+    console.log(a)
+    console.log(...rest)
+}
+values(2, 1, 5, 4, 6, 6)
+
+// spread (espalhar) permite que um objeto iterável, como uma expressão de array ou uma string seja expandido para ser usado onde zero ou mais argumentos
+
+const numbers = [1, 21, 3]
+console.log(numbers)
+// Spread
+console.log(...numbers)
+
+// Criando objeto
+const data1 = [
+    {
+        name: 'Wesley',
+        sobrenome: 'Alexandre'
+    },
+    {
+        name: 'Rodrigues',
+        sobrenome: 'Martins'
+    },
+]
+console.log(data1)
+// Utilizando spread em objetos
+console.log(...data1)
+
