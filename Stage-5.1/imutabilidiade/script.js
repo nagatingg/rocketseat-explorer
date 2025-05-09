c387d0c103e48acfb145d9c072e2bd1f73f06d9e
Wesley Alexandre <wesley.lopes14@hotmail.com>
2025-05-09 08:22:01 -0300

    Finalizando uso do deepFreeze

--- Stage-5.1/imutabilidiade/script.js
+++ Stage-5.1/imutabilidiade/script.js
@@ -15,12 +15,27 @@ Object.freeze(book)
 
 // Objeto aninhados ainda é alterado
 // book.author.name = "Joao"
-console.log(book)
 
 function deepFreeze(object){
     // Obtém um array com todas as propriedades do objeto
     const props = Reflect.ownKeys(object)
-    console.log(props)
+
+    // Itera sobre todas as propriedades do objeto
+    for (const prop of props) {
+        // Obtem o valor associado à propriedade atual.
+        const value = object[prop]
+
+        // Verifica se tem valor E se o valor é um objeto ou função
+        if (value && typeof value === "object" || typeof value === "function") {
+            deepFreeze(value)
+        }
+    }
+    return Object.freeze(object)
 }
 
+// Chama a função para congelar o objeto com Deep Freeze (congelamento profundo)
 deepFreeze(book)
+
+// Tentativa de modificar
+book.author.name = "Pedro"
+console.log(book)
